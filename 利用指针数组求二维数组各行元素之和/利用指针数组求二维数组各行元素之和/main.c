4d379c01b7107c1af932fca4f7b33bd2fe3306e2
Han <kismetfor33455@163.com>
2023-11-11 23:20:35 +0800

    Initial commit

---
+++ 利用指针数组求二维数组各行元素之和/利用指针数组求二维数组各行元素之和/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  利用指针数组求二维数组各行元素之和
+//
+//  Created by rain on 2023/11/11.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
